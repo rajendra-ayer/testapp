7788134cce27c4753b94526eba328c4a587c6b0b
Rajendra Ayer <rajendra.ayer@outlook.com>
2025-06-18 21:34:05 +1000

    Multipage

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Predictor"
+)
+
+st.title("AI APP FOR PREDICTION")
+st.write(''' Welcome to AI APP
+         
+         We do prediction house price prediction here''')
